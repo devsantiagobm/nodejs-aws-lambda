d480646ad019a16ec6b735013f4d53066c0bace2
User <sanseb290514@gmail.com>
2024-03-09 17:58:13 -0500

    feat: implement email sending with SNS
    Note that due to the use of the SES sandbox version, emails can only be sent to verified addresses

--- src/user.js
+++ src/user.js
@@ -1,4 +1,4 @@
-const { DynamoDB, SES } = require("aws-sdk")
+const { DynamoDB, SES, SNS, } = require("aws-sdk")
 const { v4 } = require("uuid")
 
 module.exports.addUser = async (event) => {
@@ -86,5 +86,98 @@ module.exports.getUsers = async (event) => {
     const usuarios = await database.scan({ TableName: "usuarios" }).promise()
 
     return { usuarios }
+}
+
+module.exports.sns = async (event) => {
+
+    
+    try {
+        const {address} = JSON.parse(event.Records[0].Sns.Message);
+
+        if(!address) return {error: "El campo address es obligatorio" }
+
+        const ses = new SES();
+
+        let emailParams = {
+            Destination: {
+                // This application uses the sandbox version of SES, so it can only send emails to addresses verified in SES
+                ToAddresses: ["sanseb290514@gmail.com", /*address*/]
+            },
+            Message: {
+                Body: {
+                    Html: {
+                        Data: `
+                            <html>
+                                <head>
+                                    <style>
+                                        body {
+                                            font-family: 'Arial', sans-serif;
+                                            background-color: #f4f4f4;
+                                            margin: 0;
+                                            padding: 20px;
+                                        }
+                                        .container {
+                                            max-width: 600px;
+                                            margin: 0 auto;
+                                            background-color: #fff;
+                                            padding: 20px 32px;
+                                            border-radius: 5px;
+                                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                        }
+                                        h1 {
+                                            color: #333;
+                                        }
+                                    </style>
+                                </head>
+                                <body>
+                                    <div class="container">
+                                        <h1>Correo usando SES y SNS</h1>
+                                        <p>Hola ${address}</p>
+                                    </div>
+                                </body>
+                            </html>
+                        `,
+                        Charset: "UTF-8"
+                    }
+                },
+                Subject: {
+                    Data: "AWS SES",
+                    Charset: "UTF-8"
+                }
+            },
+            Source: "sanseb290514@gmail.com"
+        };
+
+        await ses.sendEmail(emailParams).promise()        
+        return {
+            mensaje: "Correo enviado"
+        }
+        
+    } catch (error) {
+        console.log("ALGO PASÓ");
+        console.log(error);
+        console.log(error.message);
+        return {
+            mensaje: error.message
+        }
+
+    }
+
+}
+
+module.exports.email = async (event) => {
+    try {
+        const { address } = JSON.parse(event.body)
+        if(!address) return {error: "El campo address es obligatorio" }
+
+        const params = { TargetArn: "arn:aws:sns:us-west-2:058264440990:email", Message: JSON.stringify({address}) };
+
+        const client = new SNS()
+        await client.publish(params).promise()
+        return { mensaje: "Correo enviado" };
+
+    } catch (error) {
+        return { mensaje: error.message }
+    }
 }
 
